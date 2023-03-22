a32fe8c4eded9d1ee7e6a66ee49bc5047281b8e9
ingdolm <ing_dolm@naver.com>
2023-03-22 18:55:00 +0900

    camrin prototype - css, js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     let menuBtn = $(".menu-btn");
     let menuList = $(".menu-list");
+    menuBtn.find('div').removeClass("active");
+    menuList.removeClass("active");
     menuBtn.on("click", function(){
         menuBtn.find('div').toggleClass("active");
         menuList.toggleClass("active");
