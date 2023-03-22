ff2cf739fcfaf3a66763c5deaac2132512d3ae44
ingdolm <ing_dolm@naver.com>
2023-03-22 16:46:38 +0900

    camrin prototype page - fin

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    alert("hello world!");
-})
+    let menuBtn = $(".menu-btn");
+    let menuList = $(".menu-list");
+    menuBtn.on("click", function(){
+        menuBtn.find('div').toggleClass("active");
+        menuList.toggleClass("active");
+    });
+});
